d8df096ddaf1446d5cb9abc6d023357b7655b4dd
bauuuu1021 <bboy010554@icloud.com>
2019-02-21 11:12:59 +0800

    Impletement split part of merge sort

--- examples/merge-sort.c
+++ examples/merge-sort.c
@@ -1,86 +1,93 @@
 #include <assert.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include "../include/list.h"
-#include "../private/common.h"
+#include <stdlib.h>
+#include "list.h"
 
-static uint16_t values[256];
+#include "common.h"
 
-static void list_merge(struct list_head *head, int front, int mid, int end)
+static uint16_t values[16];
+
+static void list_combine(struct list_head *p, struct list_head *q)
 {
-    struct list_head leftList, rightList, *newList = NULL, *nextVisit;
-    int nLeft = mid - front, nRight = end - mid;  // num of **link** in sublist
-    struct list_head *lCurrent, *rCurrent, *delete;
-    int n;
-    struct listitem *leftEnd, *rightEnd;
+    struct listitem *left, *right;
+    struct list_head result;
+    INIT_LIST_HEAD(&result);
 
-    // init two sublist
-    INIT_LIST_HEAD(&leftList);
-    INIT_LIST_HEAD(&rightList);
 
-    // complete left sublist
-    n = front;
-    for (lCurrent = head->next; n > 0; lCurrent = lCurrent->next, n--)
-        ;  // find start of leftlist
-    for (; nLeft > 0; lCurrent = lCurrent->next, nLeft--)
-        list_add_tail(lCurrent, &leftList);
+    struct listitem *cur, *is;
+    while (!list_empty(p) && !list_empty(q)) {
+        left = list_first_entry(p, struct listitem, list);
+        right = list_first_entry(q, struct listitem, list);
 
-    // complete right sublist
-    n = mid;
-    for (rCurrent = head->next; n > 0; rCurrent = rCurrent->next, n--)
-        ;  // find start of rightlist
-    for (; nLeft > 0; rCurrent = rCurrent->next, nLeft--)
-        list_add_tail(rCurrent, &rightList);
+        if (left->i < right->i)
+            list_move_tail(&left->list, &result);
+        else
+            list_move_tail(&right->list, &result);
 
-    // set INT_MAX to the end of both sublist
-    leftEnd = (struct listitem *) malloc(sizeof(struct listitem));
-    rightEnd = (struct listitem *) malloc(sizeof(struct listitem));
-    leftEnd->i = INT16_MAX;
-    rightEnd->i = INT16_MAX;
-    list_add_tail(&leftEnd->list, &leftList);
-    list_add_tail(&rightEnd->list, &rightList);
+        printf("result:");
+        list_for_each_entry_safe (cur, is, &result, list) {
+            printf(" %d ", cur->i);
+        }
+        printf("\n");
+    }
 
-    // compare and insert into **testlist**
-    n = front;
-    for (lCurrent = head->next; n > 0; lCurrent = lCurrent->next, n--)
-        ;  // find start of leftlist
-    n = mid;
-    for (rCurrent = head->next; n > 0; rCurrent = rCurrent->next, n--)
-        ;  // find start of rightlist
 
-    n = end - front + 1;
-    for (delete = head; n > 0; delete = delete->next, n--)
-        list_del(delete);
-
-    while (!(list_is_singular(&leftList) && list_is_singular(&rightList))) {
-        if (list_entry(lCurrent, struct listitem, list)->i <=
-            list_entry(rCurrent, struct listitem, list)->i) {
-            nextVisit = lCurrent->next;
-            list_move_tail(lCurrent, newList);
-            lCurrent = nextVisit;
-        } else {
-            nextVisit = rCurrent->next;
-            list_move_tail(rCurrent, newList);
-            rCurrent = nextVisit;
+    if (!list_empty(p)) {
+        list_for_each_entry_safe (cur, is, p, list) {
+            list_move_tail(&cur->list, &result);
         }
     }
+    if (!list_empty(q)) {
+        list_for_each_entry_safe (cur, is, q, list) {
+            list_move_tail(&cur->list, &result);
+        }
+    }
+    printf("test\n");
+    INIT_LIST_HEAD(p);
+    list_for_each_entry_safe (cur, is, &result, list) {
+        printf("%d ", cur->i);
+        list_move_tail(&cur->list, p);
+    }
+    printf("test\n");
 }
 
-static void list_split(struct list_head *head, int front, int end)
+static void list_merge(struct list_head *head, int n)
 {
-    if (list_empty(head) || list_is_singular(head) || front >= end)
+    struct list_head p, q;
+    int i;
+
+    if (list_empty(head) || list_is_singular(head))
         return;
 
-    int mid = (int) ((front + end) / 2);
-    list_split(head, front, mid);
-    list_split(head, mid + 1, end);
-    list_merge(head, front, mid, end);
+    INIT_LIST_HEAD(&p);
+    INIT_LIST_HEAD(&q);
+
+    for (i = 0; i < n / 2; i++)
+        list_move_tail(head->next, &p);
+
+    for (i = 0; i < n - n / 2; i++)
+        list_move_tail(head->next, &q);
+
+    struct listitem *cur, *is;
+    list_for_each_entry_safe (cur, is, &p, list) {
+        printf("%d ", cur->i);
+    }
+    printf("\n");
+    list_for_each_entry_safe (cur, is, &q, list) {
+        printf("%d ", cur->i);
+    }
+    printf("\n");
+
+
+    list_merge(&p, n / 2);
+    list_merge(&q, n - n / 2);
+    list_combine(&p, &q);
 }
 
 int main(void)
 {
     struct list_head testlist;
-    struct listitem *item = NULL, *is = NULL;
+    struct listitem *item, *is = NULL;
     size_t i;
 
     random_shuffle_array(values, (uint16_t) ARRAY_SIZE(values));
@@ -93,25 +100,18 @@ int main(void)
         item = (struct listitem *) malloc(sizeof(*item));
         assert(item);
         item->i = values[i];
+        printf("%d\n", item->i);
         list_add_tail(&item->list, &testlist);
     }
 
     assert(!list_empty(&testlist));
 
-    qsort(values, ARRAY_SIZE(values), sizeof(values[0]), cmpint);
-    //////change//////
-    list_split(&testlist, 0, ARRAY_SIZE(values) - 1);
-    ///////////////////
-    i = 0;
-    list_for_each_entry_safe (item, is, &testlist, list) {
-        assert(item->i == values[i]);
-        list_del(&item->list);
-        free(item);
-        i++;
+    int n = ARRAY_SIZE(values);
+    list_merge(&testlist, n);
+
+    struct listitem *cur;
+
+    list_for_each_entry_safe (cur, is, &testlist, list) {
+        printf("[]%d ", cur->i);
     }
-
-    assert(i == ARRAY_SIZE(values));
-    assert(list_empty(&testlist));
-
-    return 0;
 }
